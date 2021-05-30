174ed0f7ac00b1f512bfa51658ba9ee4596105cc
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-05-30 16:13:25 +0200

    erster Marker fuer Stuttgart

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,8 @@ export class HomePage {
   public geoBreite = "49.014";
   public geoLaenge = "8.4043";
 
+  public marker1geoBreite = "48.775556";
+  public marker1geoLaenge = "9.182778";
 
   constructor() {}
 
