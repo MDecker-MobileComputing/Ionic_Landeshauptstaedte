4f87de0d4b62d75f6b260a3dcf3ace834cf2448d
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-05-15 14:36:34 +0200

    Neue Methode dazu(), Zuruecksetzen setzt jetzt auch Typ zurueck

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit } from '@angular/core';
 import { Component } from '@angular/core';
 
-import { GoogleMap } from '@capacitor/google-maps';
+import { GoogleMap, MapType } from '@capacitor/google-maps';
 
 // Importe eigener Klassen
 import { BundeslandUndHauptstadt } from '../bundesland-und-hauptstadt';
@@ -29,38 +29,52 @@ export class HomePage implements AfterViewInit {
                                       lng: 10.447683
                                     };
 
+  /** Standard-Zoom-Faktor. */
   readonly defaultZoomFaktor = 5.5;
+
 
   /**
    * Objekt für programmatischen Zugriff auf GoogleMaps-Instanz.
-   *
+   * <br><br>
    * siehe auch:
    * * `skipLibCheck:true` in `tsconfig.json`
    * * CSS-Klasse für GoogleMaps-Instanz in `home.page.scss`
    */
   private googleMapObjekt: GoogleMap | null = null;
 
+
   /**
    * Füllt Array mit allen 16 Landeshauptstädten. Siehe auch https://bundesland24.de/bundeslaender/
    */
   constructor() {
 
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Baden-Württemberg"     , "Stuttgart"  , false, 48.775556, 9.182778  ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Bayern"                , "München"    , false, 48.137222, 11.575556 ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Berlin"                , ""           , true , 52.518611, 13.408333 ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Brandenburg"           , "Potsdam"    , false, 52.395833, 13.061389 ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Bremen"                , ""           , true , 53.075878, 8.807311  ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Hamburg"               , ""           , true , 53.550556, 9.993333  ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Hessen"                , "Wiesbaden"  , false, 50.0825  , 8.24      ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Mecklenburg-Vorpommern", "Schwerin"   , false, 53.633333, 11.416667 ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Niedersachsen"         , "Hannover"   , false, 52.374444, 9.738611  ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Nordrhein-Westfalen"   , "Düsseldorf" , false, 51.225556, 6.782778  ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Rheinland-Pfalz"       , "Mainz"      , false, 50       , 8.271111  ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Saarland"              , "Saarbrücken", false, 49.23265 , 6.99619   ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Sachsen"               , "Dresden"    , false, 51.049259, 13.73836  ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Sachsen-Anhalt"        , "Magdeburg"  , false, 52.133333, 11.616667 ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Schleswig-Holstein"    , "Kiel"       , false, 54.32321 , 10.14019  ) );
-    this.bundeslandUndHauptstadtArray.push( new BundeslandUndHauptstadt( "Thüringen"             , "Erfurt"     , false, 50.978056, 11.029167 ) );
+    this.dazu( new BundeslandUndHauptstadt( "Baden-Württemberg"     , "Stuttgart"  , false, 48.775556, 9.182778  ) );
+    this.dazu( new BundeslandUndHauptstadt( "Bayern"                , "München"    , false, 48.137222, 11.575556 ) );
+    this.dazu( new BundeslandUndHauptstadt( "Berlin"                , ""           , true , 52.518611, 13.408333 ) );
+    this.dazu( new BundeslandUndHauptstadt( "Brandenburg"           , "Potsdam"    , false, 52.395833, 13.061389 ) );
+    this.dazu( new BundeslandUndHauptstadt( "Bremen"                , ""           , true , 53.075878, 8.807311  ) );
+    this.dazu( new BundeslandUndHauptstadt( "Hamburg"               , ""           , true , 53.550556, 9.993333  ) );
+    this.dazu( new BundeslandUndHauptstadt( "Hessen"                , "Wiesbaden"  , false, 50.0825  , 8.24      ) );
+    this.dazu( new BundeslandUndHauptstadt( "Mecklenburg-Vorpommern", "Schwerin"   , false, 53.633333, 11.416667 ) );
+    this.dazu( new BundeslandUndHauptstadt( "Niedersachsen"         , "Hannover"   , false, 52.374444, 9.738611  ) );
+    this.dazu( new BundeslandUndHauptstadt( "Nordrhein-Westfalen"   , "Düsseldorf" , false, 51.225556, 6.782778  ) );
+    this.dazu( new BundeslandUndHauptstadt( "Rheinland-Pfalz"       , "Mainz"      , false, 50       , 8.271111  ) );
+    this.dazu( new BundeslandUndHauptstadt( "Saarland"              , "Saarbrücken", false, 49.23265 , 6.99619   ) );
+    this.dazu( new BundeslandUndHauptstadt( "Sachsen"               , "Dresden"    , false, 51.049259, 13.73836  ) );
+    this.dazu( new BundeslandUndHauptstadt( "Sachsen-Anhalt"        , "Magdeburg"  , false, 52.133333, 11.616667 ) );
+    this.dazu( new BundeslandUndHauptstadt( "Schleswig-Holstein"    , "Kiel"       , false, 54.32321 , 10.14019  ) );
+    this.dazu( new BundeslandUndHauptstadt( "Thüringen"             , "Erfurt"     , false, 50.978056, 11.029167 ) );
+  }
+
+
+  /**
+   * Fügt ein Bundesland-Objekt in das Array ein.
+   *
+   * @param bundeslandUndHauptstadt Objekt mit Bundesland und Landeshauptstadt
+   */
+  private dazu( bundeslandUndHauptstadt: BundeslandUndHauptstadt ) {
+
+    this.bundeslandUndHauptstadtArray.push( bundeslandUndHauptstadt );
   }
 
 
@@ -135,7 +149,7 @@ export class HomePage implements AfterViewInit {
         zoom: this.defaultZoomFaktor
       });
 
-      //await this.googleMapObjekt.setMapType( MapType.Normal );
+      await this.googleMapObjekt.setMapType( MapType.Normal ); // falls Nutzer Ansicht auf "Satellit" geändert hat
     }
   }
 
